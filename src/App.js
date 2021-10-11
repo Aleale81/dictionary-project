6beca60dce7e82a71973ddc9e8cd1d546879f7c1
Aleale81 <69593297+Aleale81@users.noreply.github.com>
2021-10-11 14:37:33 +0200

    created  dictionary-search-form component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@
 
 import dictionary1 from "./dictionary1.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={dictionary1} className="Image" alt="dictionary" />
-				<button className="btn btn-primary">search</button>
+			<div className="container">
+				<header className="App-header">
+					<img src={dictionary1} className="Image" alt="dictionary" />
+				</header>
+				<main>
+					<Dictionary />
+				</main>
 				<footer className="Footer">
 					{" "}
-					<a href="http://" target="_blank" rel="noopener noreferrer">
+					<a
+						href="https://github.com/Aleale81/dictionary-project"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
 						Open-sourse code
 					</a>{" "}
 					by Alessandra Scarpellini{" "}
 				</footer>
-			</header>
+			</div>
 		</div>
 	);
 }
